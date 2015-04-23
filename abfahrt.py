60344e93b7c96d666035d82d9c6b7373870f9af4
gk <gabriel.kabbe@mail.de>
2015-04-23 18:48:35 +0200

    fixed bug, where new updates were initialized while updating

--- abfahrt.py
+++ abfahrt.py
@@ -3,7 +3,7 @@
 import requests as req
 import re
 import bs4
-from datetime import datetime
+from datetime import datetime, timedelta
 import ipdb
 from dateutil import parser
 import curses
@@ -11,6 +11,7 @@ import time
 import multiprocessing as mp
 import threading
 import locale
+import Queue
 #necessary for umlaut
 locale.setlocale(locale.LC_ALL,"")
 
@@ -46,8 +47,10 @@ def get_departures(routes):
 		start, dest = route
 		conns = get_next_connections(start, dest, t)
 		for c in conns:
+			if c < datetime.now():
+				c += timedelta(days=1)
 			next_departures.append((c, start, dest))
-	next_departures.sort(key=lambda x: (x[0]-datetime.now()).seconds)
+	next_departures.sort(key=lambda x: (x[0]-datetime.now()).total_seconds())
 	return next_departures
 	
 def get_departures_queue(routes, t, q):
@@ -56,12 +59,17 @@ def get_departures_queue(routes, t, q):
 		start, dest = route
 		conns = get_next_connections(start, dest, t)
 		for c in conns:
+			if c < datetime.now():
+				c += timedelta(days=1)
 			next_departures.append((c, start, dest))
-	next_departures.sort(key=lambda x: (x[0]-datetime.now()).seconds)
+	next_departures.sort(key=lambda x: (x[0]-datetime.now()).total_seconds())
 	q.put(next_departures)
 
 class CursesWindow:
-	def __init__(self):
+	def __init__(self, debug=False):
+		self.debug = debug
+		if self.debug == True:
+			self.log = open("abfahrt.log", "a")
 		self.curses_colors = [curses.COLOR_WHITE, curses.COLOR_CYAN, curses.COLOR_BLUE, 
 								curses.COLOR_GREEN, curses.COLOR_YELLOW, curses.COLOR_MAGENTA, curses.COLOR_RED]
 		self.routes = [("Triftstr.", "Büschdorf"), ("Triftstr.", "Kröllwitz"), ("Volkspark", "Rannischer Platz"), ("Volkspark", "Pfarrstr.")]
@@ -77,35 +85,45 @@ class CursesWindow:
 		pass
 	def __exit__(self, type, value, traceback):
 		curses.endwin()
+		if self.debug == True:
+			self.log.close()
 		
 	def run(self):
+		time_shift = timedelta(minutes=9)
 		time_for_update = True
 		updating = False
 		delete = False
-		q = mp.Queue()
+		q = Queue.Queue()
 		p = None
 		while 1:
 			self.myscreen.clear()
-			if time_for_update == True and p is None:
-				t = datetime.now()
+			if time_for_update == True:
+				t = datetime.now() + time_shift
 				p = threading.Thread(target=get_departures_queue, args=(self.routes, t, q))
 				p.start()
 				updating = True
 				time_for_update = False
+				if self.debug == True:
+					print >> self.log, "time for update!"
+					print >> self.log, "time is", t
 			if updating == True:
 				try:
 					self.departures = q.get(timeout=0.1)
 					p.join()
-					p = None
 					self.myscreen.addstr(0, 0, "Update finished!", curses.color_pair(4))
 					updating = False
-				except mp.queues.Empty:
+					if self.debug == True:
+						print >> self.log, "got {} from thread".format(self.departures)
+				#~ except mp.queues.Empty:
+				except Queue.Empty:
+					if self.debug == True:
+						print >> self.log, "queue was empty. keep updating"
 					self.myscreen.addstr(0, 0, "Updating...", curses.color_pair(5))
 			
 			counter = 0
-			for i, (dept_time, start, dest) in enumerate(self.departures):
-				time_left = dept_time - datetime.now()
-				time_left_sec =  int((dept_time - datetime.now()).total_seconds())
+			for dept_time, start, dest in self.departures:
+				#~ time_left = dept_time - datetime.now() - time_shift
+				time_left_sec =  int((dept_time - datetime.now() - time_shift).total_seconds())
 				#choose color depending on time left
 				if time_left_sec > 600:
 					color = curses.color_pair(4)
@@ -118,12 +136,16 @@ class CursesWindow:
 					#~ self.myscreen.addstr(counter*3 + 1, 0, "{:10} -> {:10}: {}".format(start, dest, time_left_str), color)
 					self.myscreen.addstr(counter*3 + 1, 0, "{:5} -> {:5}: {}".format(start[:5], dest[:5], time_left_str), color)
 					counter += 1
+				else:
+					if time_left_sec < -60:
+						delete = True
 			if delete == True:
 				self.myscreen.addstr(0, 0, "Deleting...", curses.color_pair(5))
 				self.departures.pop(0)
 				delete = False
-			if len(self.departures) < 12:
+			if len(self.departures) < 12 and updating == False:
 				time_for_update = True
+			#~ self.myscreen.addstr(30, 0, "dep len: {}, time for update: {}, updating: {}, delete: {}".format(len(self.departures), time_for_update, updating, delete), curses.color_pair(1))
 			self.myscreen.refresh()
 			time.sleep(1)
 		
@@ -167,6 +189,6 @@ def main(*args):
 	#~ for nd in next_departures:
 		#~ print nd[0].strftime("%H:%M"), nd[1], "->", nd[2]
 if __name__ == "__main__":
-	cw = CursesWindow()
+	cw = CursesWindow(debug=True)
 	with cw:
 		cw.run()
